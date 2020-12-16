525c4e1ab012393ea75ab5d817679960296422a7
kieranmaraj <kmaraj@my.yorku.ca>
2020-12-15 23:08:26 -0500

    Mobile + menu

--- public/audienceDrawing.js
+++ public/audienceDrawing.js
@@ -23,22 +23,6 @@ setUpCanvas();
 
 window.requestAnimationFrame(draw);
 
-document.body.addEventListener("touchstart", function (e) {
-    // if (e.target == canvas) {
-      e.preventDefault();
-    // }
-  }, false);
-  document.body.addEventListener("touchend", function (e) {
-    // if (e.target == canvas) {
-      e.preventDefault();
-    // }
-  }, false);
-  document.body.addEventListener("touchmove", function (e) {
-    // if (e.target == canvas) {
-      e.preventDefault();
-    // }
-  }, false);
-
 canvas.addEventListener('mousedown', (event)=>{
     const pos = getPointerPosition(canvas, event);
 
